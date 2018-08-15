f526fe30c91cf0e7cbbcd38a17bb88cab62fc9d3
kael_ding <Tiny@DESKTOP-0UJ5OAB>
2018-08-15 17:42:18 +0800

    更新版本

--- src/main/java/com/dreawer/customer/CustomerBootApplication.java
+++ src/main/java/com/dreawer/customer/CustomerBootApplication.java
@@ -3,6 +3,7 @@ package com.dreawer.customer;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
@@ -17,6 +18,7 @@ public class CustomerBootApplication {
 	}
 	
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
