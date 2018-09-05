77bd94184f3c2195e35bbe97a96d70cef778280b
fenrir <ygyoou88@gmail.com>
2018-09-05 15:05:24 +0800

    add member

--- src/main/java/com/dreawer/customer/CustomerBootApplication.java
+++ src/main/java/com/dreawer/customer/CustomerBootApplication.java
@@ -1,5 +1,6 @@
 package com.dreawer.customer;
 
+import com.dreawer.responsecode.rcdt.ResponseCode;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +16,7 @@ public class CustomerBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerBootApplication.class, args);
+		ResponseCode.initNamespace("cc");
 	}
 	
 	@Bean
